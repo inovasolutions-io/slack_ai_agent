4e7728d0b9d0ad52405a14dcbac60e92443fc932
Donatien Thorez <dona@inovasolutions.io>
2025-06-13 23:28:58 +1000

    Added basic openai gpt-4o-mini agent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from langgraph.prebuilt import create_react_agent
+
 import os
 
 load_dotenv()
@@ -15,13 +17,26 @@ if not bot_token or not app_token:
 
 app = App(token=bot_token)
 
+@app.event("message")
+def handle_message_events(body, logger):
+    """Handle general message events."""
+    logger.info(body)
+
 
 @app.event("app_mention")
-def handle_hello(body, say, client):
+def handle_hello(body, say):
     event = body["event"]
+    message = event["text"]
     thread_ts = event.get("thread_ts", event["ts"])
 
-    say(text="Processing...", thread_ts=thread_ts)
+    agent = create_react_agent(
+        model="openai:gpt-4o-mini",
+        tools= []
+    )
+    response = agent.invoke({"messages": [{"role": "user", "content": message}]})
+    text = response["messages"][-1].content
+
+    say(text=text, thread_ts=thread_ts)
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, app_token)

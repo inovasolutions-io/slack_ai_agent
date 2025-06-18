a120b17cf1e9e5c790f20cc3d6be3bed343952f3
Donatien Thorez <dona@inovasolutions.io>
2025-06-18 17:23:17 +1000

    Added RAG to agent

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from langgraph.prebuilt import create_react_agent
+from langchain.tools.retriever import create_retriever_tool
+from tools.search import get_vectorstore
 
 import os
 
@@ -17,6 +19,16 @@ if not bot_token or not app_token:
 
 app = App(token=bot_token)
 
+vectorstore = get_vectorstore()
+retriever_tool = create_retriever_tool(
+    vectorstore.as_retriever(),
+    name="search",
+    description="Retrieve information about the company. You will call this tool when you need to answer a question that you do not know the answer to.",
+)
+
+agent = create_react_agent(model="openai:gpt-4o-mini", tools=[retriever_tool])
+
+
 @app.event("message")
 def handle_message_events(body, logger):
     """Handle general message events."""
@@ -29,10 +41,6 @@ def handle_hello(body, say):
     message = event["text"]
     thread_ts = event.get("thread_ts", event["ts"])
 
-    agent = create_react_agent(
-        model="openai:gpt-4o-mini",
-        tools= []
-    )
     response = agent.invoke({"messages": [{"role": "user", "content": message}]})
     text = response["messages"][-1].content
 
